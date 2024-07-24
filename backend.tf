e2fc766801ee328e5b7190d6d2419d696f4a9756
Lakshmi Thungala <lakshmithungala187@gmail.com>
2024-07-24 10:06:36 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,15 @@
-
+terraform {
+  backend "s3" {
+    # The name of the S3 bucket to store the Terraform state file
+    bucket = "mys3-terraform-state"
+    
+    # The key is the path within the bucket where the state file is stored
+    key = "mykey/vpc/terraform.tfstate"
+    
+    # The AWS region where the S3 bucket is located
+    region = "us-east-1"
+    
+    # The DynamoDB table used for state locking and consistency checking
+    dynamodb_table = "mydb_table"
+  }
+}
