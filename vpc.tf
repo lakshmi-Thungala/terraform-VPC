a3283b09e6181260a3ee9a50a951f0c821c5221f
Lakshmi Thungala <lakshmithungala187@gmail.com>
2024-07-24 10:01:46 -0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,1 +1,23 @@
 
+resource "aws_vpc" "my_vpc" {
+cidr_block = var.vpcid
+
+tags = {
+    Name = "my_vpc"
+  }
+
+}
+
+resource "aws_internet_gateway" "my_gw" {
+    vpc_id = aws_vpc.my_vpc.id
+
+}
+
+resource "aws_eip" "ip" {
+}
+
+resource "aws_nat_gateway" "my_nat" {
+  allocation_id = aws_eip.ip.id
+  subnet_id     = aws_subnet.my_pub_sub1.id
+
+}
