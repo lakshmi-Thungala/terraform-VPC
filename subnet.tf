bd70da89ce60fb7e3da629c31a9dec48aa8a1451
Lakshmi Thungala <lakshmithungala187@gmail.com>
2024-07-24 10:09:05 -0400

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,1 +1,13 @@
+# Create the first public subnet
+resource "aws_subnet" "my_pub_sub1" {
+  cidr_block = var.pub_sub1_cidr  # The CIDR block for the first public subnet
+  availability_zone = var.pub_sub1_az  # The availability zone for the first public subnet
+  vpc_id = aws_vpc.my_vpc.id  # The ID of the VPC where the subnet is created
+}
 
+# Create the second public subnet
+resource "aws_subnet" "my_pub_sub2" {
+  cidr_block = var.pub_sub2_cidr  # The CIDR block for the second public subnet
+  availability_zone = var.pub_sub2_az  # The availability zone for the second public subnet
+  vpc_id = aws_vpc.my_vpc.id  # The ID of the VPC where the subnet is created
+}
