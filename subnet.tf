2217ec44ca94438219e37853b920c311a5d8b872
Lakshmi Thungala <lakshmithungala187@gmail.com>
2024-07-24 12:09:03 -0400

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,20 +1,41 @@
-# Create the first public subnet
+# Define the first public subnet
 resource "aws_subnet" "my_pub_sub1" {
-  cidr_block = var.pub_sub1_cidr  # The CIDR block for the first public subnet
-  availability_zone = var.pub_sub1_az  # The availability zone for the first public subnet
-  vpc_id = aws_vpc.my_vpc.id  # The ID of the VPC where the subnet is created
+  # CIDR block for the subnet, defining the IP address range
+  cidr_block = var.pub_sub1_cidr
+
+  # Availability zone where this subnet will be created
+  availability_zone = var.pub_sub1_az
+
+  # The VPC to which this subnet belongs
+  vpc_id = aws_vpc.my_vpc.id
+
+  # Add tags or other configurations as needed
 }
 
-# Create the second public subnet
+# Define the second public subnet
 resource "aws_subnet" "my_pub_sub2" {
-  cidr_block = var.pub_sub2_cidr  # The CIDR block for the second public subnet
-  availability_zone = var.pub_sub2_az  # The availability zone for the second public subnet
-  vpc_id = aws_vpc.my_vpc.id  # The ID of the VPC where the subnet is created
+  # CIDR block for the subnet, defining the IP address range
+  cidr_block = var.pub_sub2_cidr
+
+  # Availability zone where this subnet will be created
+  availability_zone = var.pub_sub2_az
+
+  # The VPC to which this subnet belongs
+  vpc_id = aws_vpc.my_vpc.id
+
+  # Add tags or other configurations as needed
 }
 
-# Create the private subnet
+# Define the private subnet
 resource "aws_subnet" "my_priv_sub" {
-  cidr_block = var.priv_sub_cidr  # The CIDR block for the private subnet
-  availability_zone = var.priv_sub_az  # The availability zone for the private subnet
-  vpc_id = aws_vpc.my_vpc.id  # The ID of the VPC where the subnet is created
+  # CIDR block for the subnet, defining the IP address range
+  cidr_block = var.priv_sub_cidr
+
+  # Availability zone where this subnet will be created
+  availability_zone = var.priv_sub_az
+
+  # The VPC to which this subnet belongs
+  vpc_id = aws_vpc.my_vpc.id
+
+  # Add tags or other configurations as needed
 }
